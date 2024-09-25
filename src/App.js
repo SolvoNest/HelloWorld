0207d5dc5e539c0dd73c2be91db0dd04e56c0774
SolvoNest <solvonest@gmail.com>
2024-09-25 21:43:50 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import User from './components/User';
+
+export default function App() {
+  return (
+    <div>
+      <User />
+    </div>
+  )
+}
+
